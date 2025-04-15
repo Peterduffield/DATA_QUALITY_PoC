bf081bb8426b1dbd2185ff327f8f0716314b7be7
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-04-15 16:22:28 -0400

    inital push and creation

---
+++ app.py
@@ -0,0 +1,55 @@
+from snowflake.snowpark import Session
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+
+# Create a function to connect using Snowpark
+SF_CREDENTIALS = {
+    "account": "jsa18243",
+    "user": "lkr_python_runner",
+    "password": "pythonpassword",
+    "role": "DATA_ENGINEER",
+    "warehouse": "DEMO_WH",
+    "database": "DATA_GOV_POC",
+    "schema": "POC_TABLES"
+}
+
+def create_snowflake_session():
+    return Session.builder.configs(SF_CREDENTIALS).create()
+session = create_snowflake_session()
+
+# Run SQL query
+business_glossary_tbl = session.sql("SELECT * FROM BUSINESS_GLOSSARY").to_pandas()
+data_catalog_tbl = session.sql("SELECT * FROM DATA_CATALOG").to_pandas()
+employee_tbl = session.sql("SELECT * FROM EMPLOYEE_CATALOG order by EMPLOYEE_ID asc").to_pandas()
+employee_use_case_catalog_tbl = session.sql("SELECT e.*, i.* FROM EMPLOYEE_CATALOG e LEFT JOIN USE_CASE_INVENTORY_TBL i ON e.EMPLOYEE_NAME = i.BUSINESS_STAKEHOLDER order by e.EMPLOYEE_ID asc").to_pandas()
+employee_glossary_tbl = session.sql("SELECT DISTINCT e.*,  bg.* from EMPLOYEE_CATALOG e LEFT JOIN BUSINESS_GLOSSARY bg ON bg.Data_Owner_Employee_Name = e.EMPLOYEE_NAME OR bg.Data_Steward_Employee_Name = e.EMPLOYEE_NAME WHERE e.GOVERNANCE_ROLE = 'Data Owner' or e.GOVERNANCE_ROLE = 'Data Steward'").to_pandas()
+employee_catalog_tbl = session.sql("SELECT DISTINCT e.*,  dc.* from EMPLOYEE_CATALOG e LEFT JOIN DATA_CATALOG dc ON dc.Data_Custodian = e.EMPLOYEE_NAME OR dc.Technical_Data_Steward = e.EMPLOYEE_NAME WHERE e.GOVERNANCE_ROLE = 'Data Custodian' or e.GOVERNANCE_ROLE = 'Technical Data Steward'").to_pandas()
+use_case_inventory_tbl = session.sql("SELECT * from USE_CASE_INVENTORY_TBL").to_pandas()
+
+def main():
+    st.set_page_config(layout="wide")
+    st.markdown(
+        """
+        <style>
+        .title-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
+        .title-container h1 {
+            font-size: 2.5em; /* Adjust size as needed */
+            margin-bottom: 45px; /* Spacing between title and subtitle */
+        }
+        </style>
+        <div class="title-container">
+            <h1>Data Governance</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+if __name__ == "__main__":
+    main()
