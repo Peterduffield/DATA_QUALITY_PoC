4b384f293b18549fcef3f613bcf7c945698fb829
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-04-16 17:48:55 -0400

    removing ratio

--- app.py
+++ app.py
@@ -141,9 +141,8 @@ def main():
                 st.write("")
                 st.markdown(f"""
                     <div style='text-align: center;'>
-                        <h2 style="margin-bottom: 1px;">{percent_passed:.2f}%</h2>
-                        <h5 style="margin-top: 0px; margin-bottom: 1px;">Passed</h5>
-                        <h5 style="margin-top: 0px; margin-bottom: 1px;">{passed_tests}/{total_tests}</h5>
+                        <h2 style="margin-top: 3px; margin-bottom: 1px;">{percent_passed:.2f}%</h2>
+                        <h5 style="margin-bottom: 1px;">Tests Passed</h5>
                     </div>
                 """, unsafe_allow_html=True)
              
