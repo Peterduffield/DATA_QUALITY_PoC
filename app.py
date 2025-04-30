8a75ef8b8390adb8eb878e68861ece8b43f2a89e
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-04-30 14:26:17 -0400

    Update app.py
    adding test select boxes

--- app.py
+++ app.py
@@ -102,8 +102,9 @@ def main():
         """,
         unsafe_allow_html=True
     )
-    #with st.sidebar:
-        #selected_database = st.selectbox("Select a Database:", snowflake_db_meta_source_table["database_name"].unique())
+    with st.sidebar:
+        selected_database = st.selectbox("Select a Database:", snowflake_db_meta_source_table["DATABASE_NAME"].unique())
+        selected_schema = st.selectbox("Select a Schema:", snowflake_db_meta_source_table["SCHEMA_NAME"].unique())
     st.dataframe(snowflake_db_meta_source_table)
     dq_by_table, dq_by_db, dq_by_data_soource =  st.tabs(['Data Quality by Table', 'Data Quality by DataBase', 'Data Quality by Data Source'])
     with dq_by_table:
